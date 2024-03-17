493606e89ba2f12c062bc4ae1444c11603f0ca78
John Aziz <johnaziz269@gmail.com>
2024-03-17 19:42:12 +0000

    fix tags of resource

--- infra/main.tf
+++ infra/main.tf
@@ -24,7 +24,7 @@ module "app_plan"  {
   source = "./core/host/appserviceplan"
   location  = var.location
   rg_name = azurecaf_name.rg_name.result
-  tags = { azd-env-name : var.environment_name, azd-service-name: "python-app"}
+  tags = { azd-env-name : var.environment_name }
   resource_token = local.resource_token
   sku_name = "B1"
   os_type = "Linux"
